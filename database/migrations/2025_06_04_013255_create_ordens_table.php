9c3198450de18d3c23ab09ed0cc700bc8343c9a2
AlexCast11 <cn20002@ues.edu.sv>
2025-09-11 22:15:50 -0600

    Fix migracion ordenes

--- database/migrations/2025_06_04_013255_create_ordens_table.php
+++ database/migrations/2025_06_04_013255_create_ordens_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('ordens', function (Blueprint $table) {
+        Schema::create('ordenes', function (Blueprint $table) {
             $table->id();
         $table->foreignId('cliente_id')->constrained()->onDelete('cascade');
         $table->decimal('total', 8, 2)->default(0);
@@ -29,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('ordens');
+        Schema::dropIfExists('ordenes');
     }
 };
